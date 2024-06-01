8d9f86437d484ac0f157c09758e81da16d4b6719
chendatorch <chendatorch@gmail.com>
2024-06-01 10:04:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <title>PHP PROJECT</title>
+<?php 
+    $host=getenv('MYSQL_HOST', true) ?: 'localhost';
+    $db=getenv('MYSQL_DB', true) ?:'mydb';
+    $username=getenv('MYSQL_USER', true) ?: 'root';
+    $password=getenv('MYSQL_PASSWORD', true) ?: 'root';
+    $port=getenv("MYSQL_PORT", true) ?: '8003';
+    $conn=new mysqli($host,$db,$username,$password,$port);
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
+    echo "Connection is established";
+?>
+</html>
