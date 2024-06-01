e7394d3b0341d72e30d4fdd08c9b48f32e596745
chendatorch <chendatorch@gmail.com>
2024-06-01 19:13:44 +0700

    add new docker file

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
     <title>PHP PROJECT</title>
 <?php 
     $host=getenv('MYSQL_HOST', true) ?: 'localhost';
-    $db=getenv('MYSQL_DB', true) ?:'mysqldb';
-    $username=getenv('MYSQL_USER', true) ?: 'root';
-    $password=getenv('MYSQL_PASSWORD', true) ?: 'root';
-    $port=getenv("MYSQL_PORT", true) ?: '8003';
+    $db=getenv('MYSQL_DATABASE', true) ?:'mysqldb';
+    $username=getenv('MYSQL_USER', true) ?: 'admin';
+    $password=getenv('MYSQL_PASSWORD', true) ?: '123';
+    $port=getenv("MYSQL_PORT", true) ?: '9001';
     $conn=new mysqli($host,$db,$username,$password,$port);
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
