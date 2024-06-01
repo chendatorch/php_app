d657196de0f482a44d322867ab85724c2827d000
chendatorch <chendatorch@gmail.com>
2024-06-01 21:11:52 +0700

    add new docker

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     $db=getenv('MYSQL_DATABASE', true) ?:'mydb';  
     $username=getenv('MYSQL_USER', true) ?: 'admin';
     $password=getenv('MYSQL_PASSWORD', true) ?: 'admin123';
-    $port=getenv("MYSQL_PORT", true) ?: '9001';
+    $port=getenv("MYSQL_PORT", true) ?: '3306';
     $conn=new mysqli($host,$db,$username,$password,$port);
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
